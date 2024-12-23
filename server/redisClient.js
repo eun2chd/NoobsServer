dc3243957e7bedb7bb5eb1970c024695dc9b6863
Ubuntu <ubuntu@ip-172-31-46-36.ap-northeast-2.compute.internal>
2024-12-23 02:07:35 +0000

    redis

--- server/redisClient.js
+++ server/redisClient.js
@@ -1,8 +1,9 @@
 import Redis from 'ioredis';
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
   connectTimeout: 10000,
 });
 
